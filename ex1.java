7859fd58c1a68afb267a90565f5548a7045d0512
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-10-19 02:04:54 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,26 @@
+//SOMA DE NUMEROS
+package listaDeExercicios;
+import java.util.*;
+
+public class ex1 {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner( System.in);
+		int n1,n2,sum;
+		
+		System.out.println("Digite um numero o primeiro numero: ");
+		n1 = sc.nextInt();
+		System.out.println("Digite o segundo número: ");
+		n2 = sc.nextInt();
+		
+		sum = (n1+n2);		
+		System.out.print("O resultado é: " +sum);			
+		
+		
+	}
+
+}
+
+
+
+
