597ac88ac26d6f13a54521d33b1a64f6f68cd406
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-11-04 12:39:05 -0300

    Add files via upload

---
+++ While2.java
@@ -0,0 +1,19 @@
+// Realizar a contagem ate 100 apenas dos numeros pares.
+package listaDeExercicios;
+
+public class While2 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int pares = 100;		
+		
+	System.out.println("Números pares são: ");
+	//System.out.println(i);
+	
+	while ( i <= pares) {
+		
+		System.out.print(" " +i);
+		i=i+2;
+	}
+  }
+}
