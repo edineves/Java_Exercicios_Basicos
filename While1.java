6991ccf1ddc724fbdc9f94d3c492d437756e14b5
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-11-04 12:26:59 -0300

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,16 @@
+package listaDeExercicios;
+
+public class While1 {
+
+	public static void main(String[] args) {
+		int i =1 ;
+		int max = 100;
+		
+	System.out.println("contar até : "+max);
+	
+	while (i<max) {
+		System.out.println(" Os valores são: " +i);
+		i++;
+  }
+ }
+}
