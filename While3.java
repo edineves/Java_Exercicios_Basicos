67503b6e1b9f4fe36b261e11fa0a064e68d79de1
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-11-04 12:55:39 -0300

    Add files via upload

---
+++ While3.java
@@ -0,0 +1,17 @@
+//Realizar a contagem ate 100 apenas dos números ímpares.
+package listaDeExercicios;
+
+public class While3 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int impar = 101;
+		
+	System.out.println("Os numeors impares são: " );
+	
+	while ( i < impar) {
+		System.out.print( " " + i );
+		i=i+2;
+  }
+ }
+}
