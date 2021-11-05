c3564f7bffdc312f9297ad60fe18554201e37219
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-11-05 17:38:58 -0300

    Add files via upload

---
+++ HorarioSistema.java
@@ -0,0 +1,15 @@
+//
+package listaDeExercicios;
+import java.util.Date;
+
+ public class HorarioSistema {
+
+	public static void main(String[] args) {
+		
+	Date relogio = new Date();
+		
+	System.out.print("A hora do atualizada em seu sistema  é " +relogio.toString());
+	
+	}
+
+}
