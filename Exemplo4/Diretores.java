0995570b4a4b1b613b5e4984a7f03d029bd29be5
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2021-11-20 15:39:28 -0300

    Add files via upload
    Criando Classes e Heranças

---
+++ Exemplo4/Diretores.java
@@ -0,0 +1,12 @@
+//Informações dos Diretores 
+package Exemplo4;
+
+public class Diretores extends Pessoas {
+	public float salLiquido (float x) {
+	float res;
+	res = x-(x*0.25f);	
+	
+	return res;
+	
+  }	
+}
---
+++ Exemplo4/Funcionarios.java
@@ -0,0 +1,11 @@
+//Informações dos funcionários.
+package Exemplo4;
+
+public class Funcionarios extends Pessoas {
+	public float salLiquido ( float x) {
+		float res;
+		res = x*0.90f;
+		
+		return res;
+	}
+}
